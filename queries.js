1ad0d9297270dfd3e47bba3f1ae32b49ae4064ac
manucosta <manucos94@gmail.com>
2017-06-15 20:48:59 -0300

    Cambios menores

--- queries.js
+++ queries.js
@@ -4,13 +4,15 @@ r.db('registroHistorico').table('Enfrentamiento').filter({'id_campeonato': 1}).g
         }).count();
 
 //2.2 La cantidad de medallas por nombre de escuela en toda la historia.
-// Podemos asumir/retringir que los nombres de las escuelas son todos diferentes? En ese caso se puede simplificar bastante esto
+// Asumiendo que las escuelas pueden repetir nombres
 r.db('registroHistorico').table('Escuela').group('nombre').pluck('medallistas').sum(function(m) {
   return m.count()
 });
+// Asumiendo que las escuelas tienen todos nombres distintos
+r.db('registroHistorico').table('Escuela').pluck('medallistas').count();
 
 //2.3 Para cada escuela, el campeonato donde ganó más medallas
-// En este y en el anterior usamos fuertemente que la escuela solo guarda a los competidores que ganaron medallas.
+// En este y en el anterior usamos fuertemente que la escuela solo guarda a los competidores que ganaron medallas (validado con Gerardo).
 r.db('registroHistorico').table('Escuela').pluck('medallistas').group('id_campeonato').count().max();
 
 //2.4 Los arbitros que participaron en al menos 4 campeonatos
